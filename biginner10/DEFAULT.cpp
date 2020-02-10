becafdd7c42c345ae29f2fbfe2abe82f2ea293b5
yushi-hosokawa <yushi.hosokawa.9g@stu.hosei.ac.jp>
2020-02-10 17:42:30 +0900

    152 qE 解けず

--- biginner10/DEFAULT.cpp
+++ biginner10/DEFAULT.cpp
@@ -2,8 +2,6 @@
 #define rep(i,n) for (int i= 0;i<(n);++i)
 using namespace std;
 typedef long long ll;
-typedef pair<int,int> P;
-
 
 int main(){
 
---
+++ biginner10/cpp_map.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i= 0;i<(n);++i)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+
+
+//mapはkey value構造を持つクラス
+//keyを呼べばvalueが帰ってくるよ
+int main(){
+  //map関数を定義
+  map<string ,int> mp;
+  //keyにabc valueに123を登録
+  mp["abc"] =123;
+  //abcを出力
+  cout <<mp["abc"]<<endl;
+
+//特に作ってないkeyを入力すると、valueとして０が入っている
+//0に1を足すので1が出力
+  cout <<(mp["add"]+1)<<endl;
+  cout <<mp.at("abc")<<endl;
+}
